439072f672a3c9d05d3cb617424c7168147c5ae5
A98H73 <singh002abhijeet@gmail.com>
2022-05-26 11:34:10 +0530

    Finally Finished

--- lib/FrontEnd/screens/RegisterUsers.dart
+++ lib/FrontEnd/screens/RegisterUsers.dart
@@ -68,172 +68,183 @@ class _RegisterUsersPageState extends State<RegisterUsersPage> {
               icon: Icon(Icons.logout)),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(
-          top: 40,
-          left: 20,
-          right: 20,
-        ),
-        child: GridView(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            mainAxisSpacing: 16.0,
-            crossAxisSpacing: 16.0,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Center(
+              child: Container(
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    top: 40,
+                    left: 20,
+                    right: 20,
+                  ),
+                  child: GridView(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 16.0,
+                      crossAxisSpacing: 16.0,
+                    ),
+                    scrollDirection: Axis.vertical,
+                    children: [
+                      Material(
+                        color: HexColor('#283b71'),
+                        //borderRadius: BorderRadius.circular(25.0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18.0),
+                          side: BorderSide(color: Colors.white),
+                        ),
+                        child: InkWell(
+                          splashColor: Colors.black,
+                          child: Container(
+                            child: const Text(
+                              'Add User',
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            alignment: Alignment.center,
+                          ),
+                          onTap: () {
+                            Navigator.pushNamed(context, "/registeruser");
+                          },
+                        ),
+                      ),
+                      Material(
+                        color: HexColor('#283b71'),
+                        //borderRadius: BorderRadius.circular(25.0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18.0),
+                          side: BorderSide(color: Colors.white),
+                        ),
+                        child: InkWell(
+                          splashColor: Colors.black,
+                          child: Container(
+                            child: const Text(
+                              'Remove User',
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            alignment: Alignment.center,
+                          ),
+                          onTap: () {
+                            Navigator.pushNamed(context, "/deleteuser");
+                          },
+                        ),
+                      ),
+                      // Material(
+                      //   color: HexColor('#283b71'),
+                      //   //borderRadius: BorderRadius.circular(25.0),
+                      //   shape: RoundedRectangleBorder(
+                      //     borderRadius: BorderRadius.circular(18.0),
+                      //     side: BorderSide(color: Colors.white),
+                      //   ),
+                      //   child: InkWell(
+                      //     splashColor: Colors.black,
+                      //     child: Container(
+                      //       child: const Text(
+                      //         'Add Admin',
+                      //         style: TextStyle(
+                      //           fontSize: 18,
+                      //           color: Colors.white,
+                      //           fontWeight: FontWeight.bold,
+                      //         ),
+                      //       ),
+                      //       alignment: Alignment.center,
+                      //     ),
+                      //     onTap: () {
+                      //       Navigator.pushNamed(context, "/registeradmin");
+                      //     },
+                      //   ),
+                      // ),
+                      // Material(
+                      //   color: HexColor('#283b71'),
+                      //   //borderRadius: BorderRadius.circular(25.0),
+                      //   shape: RoundedRectangleBorder(
+                      //     borderRadius: BorderRadius.circular(18.0),
+                      //     side: BorderSide(color: Colors.white),
+                      //   ),
+                      //   child: InkWell(
+                      //     splashColor: Colors.black,
+                      //     child: Container(
+                      //       child: const Text(
+                      //         'Remove Admin',
+                      //         style: TextStyle(
+                      //           fontSize: 18,
+                      //           color: Colors.white,
+                      //           fontWeight: FontWeight.bold,
+                      //         ),
+                      //       ),
+                      //       alignment: Alignment.center,
+                      //     ),
+                      //     onTap: () {
+                      //       Navigator.pushNamed(context, "/deleteadmin");
+                      //     },
+                      //   ),
+                      // ),
+                      Material(
+                        color: HexColor('#283b71'),
+                        //borderRadius: BorderRadius.circular(25.0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18.0),
+                          side: BorderSide(color: Colors.white),
+                        ),
+                        child: InkWell(
+                          splashColor: Colors.black,
+                          child: Container(
+                            child: const Text(
+                              'Add Doctor',
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            alignment: Alignment.center,
+                          ),
+                          onTap: () {
+                            Navigator.pushNamed(context, "/registerdoctor");
+                          },
+                        ),
+                      ),
+                      Material(
+                        color: HexColor('#283b71'),
+                        //borderRadius: BorderRadius.circular(25.0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18.0),
+                          side: BorderSide(color: Colors.white),
+                        ),
+                        child: InkWell(
+                          splashColor: Colors.black,
+                          child: Container(
+                            child: const Text(
+                              'Remove Doctor',
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            alignment: Alignment.center,
+                          ),
+                          onTap: () {
+                            Navigator.pushNamed(context, "/deletedoctor");
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
           ),
-          scrollDirection: Axis.vertical,
-          children: [
-            Material(
-              color: HexColor('#283b71'),
-              //borderRadius: BorderRadius.circular(25.0),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(18.0),
-                side: BorderSide(color: Colors.white),
-              ),
-              child: InkWell(
-                splashColor: Colors.black,
-                child: Container(
-                  child: const Text(
-                    'Add User',
-                    style: TextStyle(
-                      fontSize: 18,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  alignment: Alignment.center,
-                ),
-                onTap: () {
-                  Navigator.pushNamed(context, "/registeruser");
-                },
-              ),
-            ),
-            Material(
-              color: HexColor('#283b71'),
-              //borderRadius: BorderRadius.circular(25.0),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(18.0),
-                side: BorderSide(color: Colors.white),
-              ),
-              child: InkWell(
-                splashColor: Colors.black,
-                child: Container(
-                  child: const Text(
-                    'Remove User',
-                    style: TextStyle(
-                      fontSize: 18,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  alignment: Alignment.center,
-                ),
-                onTap: () {
-                  Navigator.pushNamed(context, "/deleteuser");
-                },
-              ),
-            ),
-            Material(
-              color: HexColor('#283b71'),
-              //borderRadius: BorderRadius.circular(25.0),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(18.0),
-                side: BorderSide(color: Colors.white),
-              ),
-              child: InkWell(
-                splashColor: Colors.black,
-                child: Container(
-                  child: const Text(
-                    'Add Admin',
-                    style: TextStyle(
-                      fontSize: 18,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  alignment: Alignment.center,
-                ),
-                onTap: () {
-                  Navigator.pushNamed(context, "/registeradmin");
-                },
-              ),
-            ),
-            Material(
-              color: HexColor('#283b71'),
-              //borderRadius: BorderRadius.circular(25.0),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(18.0),
-                side: BorderSide(color: Colors.white),
-              ),
-              child: InkWell(
-                splashColor: Colors.black,
-                child: Container(
-                  child: const Text(
-                    'Remove Admin',
-                    style: TextStyle(
-                      fontSize: 18,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  alignment: Alignment.center,
-                ),
-                onTap: () {
-                  Navigator.pushNamed(context, "/deleteadmin");
-                },
-              ),
-            ),
-            Material(
-              color: HexColor('#283b71'),
-              //borderRadius: BorderRadius.circular(25.0),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(18.0),
-                side: BorderSide(color: Colors.white),
-              ),
-              child: InkWell(
-                splashColor: Colors.black,
-                child: Container(
-                  child: const Text(
-                    'Add Doctor',
-                    style: TextStyle(
-                      fontSize: 18,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  alignment: Alignment.center,
-                ),
-                onTap: () {
-                  Navigator.pushNamed(context, "/registerdoctor");
-                },
-              ),
-            ),
-            Material(
-              color: HexColor('#283b71'),
-              //borderRadius: BorderRadius.circular(25.0),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(18.0),
-                side: BorderSide(color: Colors.white),
-              ),
-              child: InkWell(
-                splashColor: Colors.black,
-                child: Container(
-                  child: const Text(
-                    'Remove Doctor',
-                    style: TextStyle(
-                      fontSize: 18,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  alignment: Alignment.center,
-                ),
-                onTap: () {
-                  Navigator.pushNamed(context, "/deletedoctor");
-                },
-              ),
-            ),
-          ],
-        ),
+        ],
       ),
     ));
   }
